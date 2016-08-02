42fa8b0b1363457233debccdd899553111e6fb19
chad <eesql@foxmail.com>
2016-08-02 15:16:11 +0800

    optimize sse code

--- app/controllers/DashboardV3.scala
+++ app/controllers/DashboardV3.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 /**
   * Created by elainetuang on 6/16/16.
   */
-class DashboardV3 extends Controller{
+class DashboardV3 extends Controller {
   def index = Action {
     Ok( views.html.index2.render() )
   }
